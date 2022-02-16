5194c3dfd27db97eaa52f610c19627a056cfc777
Brad Messenger <wbmessenger@gmail.com>
2022-02-16 20:36:57 +0000

    Tracking Code added

--- header.php
+++ header.php
@@ -16,7 +16,15 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
-
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5Q32HPPJLN"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-5Q32HPPJLN');
+    </script>
     <?php wp_head(); ?>
 </head>
 
